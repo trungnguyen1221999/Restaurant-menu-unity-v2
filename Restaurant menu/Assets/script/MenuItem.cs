3e8f8fd2c4381df30e80295c5f436ef649c5a46b
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-10-20 11:08:57 +0300

    create pizza ui

--- Restaurant menu/Assets/script/MenuItem.cs
+++
@@ -1,8 +0,0 @@
-﻿[System.Serializable]
-public class MenuItem
-{
-    public string name;
-    public float price;
-    public string description;
-    public string[] tags;  // ví dụ: "vegetarian", "gluten-free"
-}
