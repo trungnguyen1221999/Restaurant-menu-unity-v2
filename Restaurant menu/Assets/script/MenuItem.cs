c31bfe2389d5fb23f976d82b023e59700c0d4d80
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-10-20 10:04:37 +0300

    create ui and stuctuce menu

---
+++ Restaurant menu/Assets/script/MenuItem.cs
@@ -0,0 +1,8 @@
+﻿[System.Serializable]
+public class MenuItem
+{
+    public string name;
+    public float price;
+    public string description;
+    public string[] tags;  // ví dụ: "vegetarian", "gluten-free"
+}
